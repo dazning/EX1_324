6880eb06f185ad729748bed351a582e247d14fb2
dazning <38385664+dazning@users.noreply.github.com>
2023-10-11 05:01:19 -0400

    Add files via upload

---
+++ P5_324/carrera.mv.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+
+<?php include 'header.php' ?>
+
+<section>
+    <br>
+    <div class="container-fluid" style="border-left:25px solid #5283dE;">
+      <h1 style="color: #5283dE;">Nuestra Misión, Visión y Objetivos</h1>
+      <h1 class="text-white">Carrera de Informática</h1>
+    </div>
+    <hr>
+    <div class="container">
+      <div class="container">
+        <img src="images/valores.jpg"  class="img-thumbnail" alt="" style="width:450px;border-radius:15px; float:right;margin: 10px; margin-top: 10px;">
+      </div>
+      <br>
+      <p class="text-justify" style="color: #5283dE; font-size: 25px;">
+        MISIÓN
+      </p>
+      <p class="text-justify text-white" style="line-height: 30px;">
+        Formar profesionales idóneos con calidad humana, ética, valores, excelencia científica, compromiso social, capacidad crítica y creativa para potenciar el desarrollo de la ciencia y la tecnología en el área de la Informática en concordancia con los requerimientos de la sociedad y sus instituciones, desempeñándose con éxito en el ámbito regional, nacional e internacional.
+      </p>
+      <br>
+      <p class="text-justify" style="color: #5283dE; font-size: 25px;">
+        VISIÓN
+      </p>
+      <p class="text-justify text-white" style="line-height: 30px;">
+        Ser la unidad académica líder a nivel nacional y un referente de alto nivel en la formación de profesionales del área de la Informática, que aporta a la región y el país no solo con sus graduados sino también con proyectos de investigación y extensión de alto impacto relacionados con ciencia y tecnología.
+      </p>
+      <br>
+      <p class="text-justify" style="color: #5283dE; font-size: 25px;">
+        OBJETIVOS
+      </p>
+      <p class="text-justify">
+        <ul style="margin-left: 40px;line-height: 30px;">
+          <li class="text-white" style="list-style: outside; ">Formar profesionales altamente calificados en ciencia y tecnología del área de la Informática para responder a las necesidades de la sociedad.</li>
+          <li class="text-white" style="list-style: outside;">Conducir proyectos de Investigación científica en el área de la Informática.</li>
+          <li class="text-white" style="list-style: outside;">Conducir proyectos de Interacción social en la región y el país.</li>
+          <br>
+        </ul>
+      </p>
+      <div class="container">
+        <img src="images/valores1.jpg" class="img-thumbnail" alt=""
+        style="width:450px; border-radius:15px; float:right;margin: 10px; margin-top: 10px;">
+      </div>
+      <p class="text-justify" style="color: #5283dE; font-size: 25px;">
+        VALORES
+      </p>
+      <p class="text-justify">
+        En la carrera se fomentan los siguientes valores:
+      </p>
+      <p class="text-justify">
+        <ul style="margin-left: 40px;line-height: 30px;">
+          <li class="text-white" style="list-style: outside; ">Honestidad</li>
+          <li class="text-white" style="list-style: outside;">Ética</li>
+          <li class="text-white" style="list-style: outside;">Responsabilidad</li>
+          <li class="text-white" style="list-style: outside;">Equidad e igualdad</li>
+          <li class="text-white" style="list-style: outside;">Respeto a la diversidad cultural</li>
+          <li class="text-white" style="list-style: outside;">Respeto por el medio ambiente</li>
+          <li class="text-white" style="list-style: outside;">Rigor profesional y científico</li>
+          <li class="text-white" style="list-style: outside;">Puntualidad</li>
+          <br>
+        </ul>
+      </p>
+    </div>
+  </section>
+
+
+
+
+  <!--Fin código-->
+
+
+
+
+  <?php include 'foot.php' ?>
+  <!-- End Google Map -->
+</body>
+
+</html>
